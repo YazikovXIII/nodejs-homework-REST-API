90afec7875feac954deb4fabb76ac5a19bf07083
Влад Язиков <vladzaraza@gmail.com>
2023-08-21 12:09:43 +0300

    dotenv.config() in app.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 const { DB_HOST } = process.env;
-// "mongodb+srv://vladzaraza:1HB8tz7p48pNKe0z@cluster0.4fiikna.mongodb.net/GoITNodeJS&DB?retryWrites=true&w=majority";
 
 mongoose.set("strictQuery", true);
 
