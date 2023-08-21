bc50b65576f9f690b409fe6b8b78f80d069b1c01
Влад Язиков <vladzaraza@gmail.com>
2023-08-21 11:51:36 +0300

    process.env DB_HOST

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const DB_HOST =
-  "mongodb+srv://vladzaraza:1HB8tz7p48pNKe0z@cluster0.4fiikna.mongodb.net/GoITNodeJS&DB?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
+// "mongodb+srv://vladzaraza:1HB8tz7p48pNKe0z@cluster0.4fiikna.mongodb.net/GoITNodeJS&DB?retryWrites=true&w=majority";
 
 mongoose.set("strictQuery", true);
+
+// console.log(process.env);
 
 mongoose
   .connect(DB_HOST)
@@ -16,6 +18,4 @@ mongoose
     process.exit(1);
   });
 
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
+// https://goit-nodejs-db-api.onrender.com/api/contacts
